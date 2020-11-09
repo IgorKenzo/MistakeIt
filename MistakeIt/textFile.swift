5634870eb9810cebbdeeef262b573df4f83bfaa3
IgorKenzo <igor.kenzo.m@gmail.com>
2020-11-09 14:26:10 -0300

    testando

--- MistakeIt/textFile.swift
+++ MistakeIt/textFile.swift
@@ -1,10 +1,10 @@
-
 //
 //  HintsFile.swift
 //  MistakeIt
 //
 //  Created by marcelo frost marchesan on 27/10/20.
 //
+
 import Foundation
 
 let leveltexts = ["lamp": "O erro é um aprendizado e não apenas um erro!",
@@ -28,10 +28,3 @@ let levelcomplete = ["lamp": "Foram diversas as tentativas de criar a lâmpada i
                      "penicillin": "Pesquisando como combater bactérias em ferimentos, o médico Alexander Fleming cometeu um erro em suas pesquisas em virtude de seu cansaço. Ele esqueceu o material pesquisado aberto e fora de conservação, o que fez com que fungos aparecessem no material de pesquisa. Posteriormente, percebeu que onde o fungo havia se espalhado, as bactérias não se reproduziam. Anos mais tarde, a descoberta de Fleming permitiu a outros cientistas desenvolverem uma forma de combater as bactérias a partir do material coletado de fungos, levando à invenção da penicilina.",
                      "docedeleite": " Em 1829 uma cozinheira preparava um doce de leite com açúcar para servir a convidados de uma festa. Ao se distrair, a cozinheira deixou os ingredientes no fogo por mais tempo do que a receita exigia, criando uma mistura mais escura e espessa. Sem tempo de corrigir o erro, a mistura foi servida aos convidados, provando-se um verdadeiro sucesso. Foi inventado, assim, o doce de leite na Argentina.",
                      "pacemaker": "O marca passo foi inventado por Wilson Greatbatch em 1956. Inicialmente o pesquisador estava construindo um dispositivo para medir os movimentos cardíacos. Ao trabalhar em seu dispositivo, porém, removeu acidentalmente um dos fios do aparelho, o que fez com que o aparelho emitisse sinais elétricos em um ritmo semelhante às batidas do coração humano. Isso permitiu criar um aparelho para induzir o coração aos batimentos corretos a partir dos sinais elétricos."]
-
-
-let levelcomplete2 = ["lamp": "Foram diversas as tentativas de criar a lâmpada incandescente até que Thomas Alva Edison conseguiu chegar em modelo final em 1879. O próprio inventor disse ter feito mais de 100 testes diferentes antes de se chegar ao resultado final. Uma das frases atribuídas ao inventor é ''Eu aprendi muito mais com os meus erros do que com meus acertos''",
-                     "papersticker": "O inventor Spencer Silver procurava criar uma cola super aderente, mas conseguiu apenas uma cola de pouca aderência. Algum tempo depois, Arthur Fry, observou que a cola permitia aderir folhas de papel delicamente. Juntos, viram o potencial comercial do produto e inventaram um papel adesivado que pode ser colocado e descolado",
-                     "penicillin": "Pesquisando como combater bactérias em ferimentos, o médico Alexander Fleming esqueceu o material pesquisado aberto e fora de conservação, fazendo com que fungos aparecessem no material. Fleming percebeu que onde o fungo havia se espalhado, as bactérias não se reproduziam. A partir dessa descoberta, outros cientistas criaram um produto a base daquele fungo que eliminava as bactérias, inventando a penicilina.",
-                     "docedeleite": "Em 1829 na Argentina, uma cozinheira preparava um doce feito de leite e açúcar para servir em uma festa. Por erro, os ingredientes cozinharam mais tempo do que o previsto, criando uma mistura mais escura e espessa. Apesar do erro, a mistura foi chamada de doce de leite e foi servida aos convidados, provando-se um verdadeiro sucesso",
-                     "pacemaker": "O marca passo foi inventado por Wilson Greatbatch em 1956. Inicialmente o pesquisador trabalhava em um dispositivo para medir os movimentos cardíacos, mas, ao acidentalmente remover um dos fios elétricos, o aparelho passou a emitir sinais elétricos em um ritmo semelhante ao do coração. O erro transformou-se em um equipamento que induz batimentos corretos no coração a partir de sinais elétricos"]
