ae8993e267577604b432db827c7d69f4af085148
IgorKenzo <igor.kenzo.m@gmail.com>
2020-11-09 15:15:12 -0300

    arrumando merge

---
+++ MistakeIt/navigation/Navigation.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation.swift
+//  MistakeIt
+//
+//  Created by IgorMiyamoto on 05/11/20.
+//
+import Foundation
+
+    
+let MAXLEVEL = 4
+
+enum LevelState : Int {
+    case lamp
+    case marc
+    case postit
+    case peni
+    case doceleite
+}
