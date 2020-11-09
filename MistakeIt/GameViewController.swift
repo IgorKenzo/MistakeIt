51f09fc40e93dcafa0c9343d18295f92f5ef2b1d
IgorKenzo <igor.kenzo.m@gmail.com>
2020-11-09 14:44:01 -0300

    Nao esta mais quebrado
    Funcionando, acredito eu, abre na fase da lampada, precisa criar um .sks para as fases, Falta navegação

--- MistakeIt/GameViewController.swift
+++ MistakeIt/GameViewController.swift
@@ -5,17 +5,44 @@
 //  Created by IgorMiyamoto on 09/11/20.
 //
 
-import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "FaseLampadaScene") {
+                // Set the scale mode to scale to fit the window
+                
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
     }
-    
 
+    override var shouldAutorotate: Bool {
+        return true
+    }
 
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
